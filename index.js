d3273df0b2993bfeb35226dd20ad51cc91430399
kuchbhimingwal <shubhammingi@gmail.com>
2024-01-19 06:28:51 -0500

    student json added

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+
+const student = {
+  "name": "John Doe",
+  "age": 25,
+  "email": "john.doe@example.com",
+  "isStudent": false,
+  "address": {
+    "street": "123 Main Street",
+    "city": "Anytown",
+    "country": "USA"
+  },
+  "hobbies": ["reading", "coding", "traveling"],
+  "scores": {
+    "math": 90,
+    "english": 85,
+    "history": 75
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +33,9 @@ app.get('/login', (req,res) =>{
   res.send('<h1>please login at chai or code</h1>')
 })
 
+app.get('/student',(req, res)=>{
+  res.json(student)
+})
 app.get('/youtube', (req,res) => {
   res.send("<h2>Chai or code</h2>")
 })
